f3bfe600af16871280284d2a66e4d5b61e13d9bd
Andrius Rainys <andraitis@gmail.com>
2017-02-02 17:37:37 +0000

    new commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+
+var useragent = require('useragent');
+
+var port = process.env.PORT || 3500;
+
+app.listen(port, function(){
+  console.log("Listening on port: " + port);
+});
+
+app.get('/', function(req, res) {
+  var agent = useragent.parse(req.headers['user-agent']);
+
+  var ipAddr = req.headers["x-forwarded-for"];
+  if (ipAddr){
+    var list = ipAddr.split(",");
+    ipAddr = list[list.length-1];
+  } else {
+    ipAddr = req.connection.remoteAddress;
+  }
+
+  res.json({
+    ip: ipAddr,
+    "language": req.headers['accept-language'].split(',')[0],
+    OS: agent.os.family
+  });
+});
